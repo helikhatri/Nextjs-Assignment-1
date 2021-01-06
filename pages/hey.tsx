847b894af57e4fc06c4e2b6e9daf2fb58e7f365a
H k <helikhatri64@gmail.com>
2021-01-06 09:45:32 +0530

    initial commit

---
+++ pages/hey.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import styles from '../Home.module.css'
+
+const index = () => {
+  <div>
+    <h1>Hello next</h1>
+    <a href='/pages/Login'>Login</a>
+  </div>
+}
+export default index;
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import styles from '../Home.module.css'
+
+const index = () => {
+  <div>
+    <h1>Hello world</h1>
+  </div>
+}
+export default index;
