64c2263ed27df17a786636f9b4f83335b98e4fc3
H k <helikhatri64@gmail.com>
2021-01-06 10:03:15 +0530

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,10 @@ import Head from 'next/head'
 import styles from '../Home.module.css'
 
 const index = () => {
+  return(
   <div>
     <h1>Hello world</h1>
   </div>
+  );
 }
 export default index;
